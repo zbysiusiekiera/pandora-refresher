a3b2e6c2af20a6167182c8f11af604924d2d8e07
jkochman <jkochman@mt-silesia.com>
2015-09-25 11:26:48 +0200

    Added brief explanation, cleared src files

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # pandora-refresher
 Chrome extension which autamatically refreshes Pandora Radio when 'Are you still listening' question appears.
+
+Periodically each 10s it will search document for button which indicates that Pandora thinks you're away and not listening. In such case it will simply reload the page.
--- src/pandora_refresher.js
+++ src/pandora_refresher.js
@@ -1,14 +1,11 @@
 (function() {
-	function checkForButton() {
-		console.log('Checking for button');
-		if (document.getElementById('still_listening_ignore')) {
-			console.log('Refreshing!');
-			window.location = 'http://www.pandora.com';
-		}
-	}
-	
-	setInterval(checkForButton, 10000);
-	
+  function checkForButton() {
+    console.log('Checking for button');
+    if (document.getElementById('still_listening_ignore')) {
+      console.log('Refreshing!');
+      window.location = 'http://www.pandora.com';
+    }
+  }
+  setInterval(checkForButton, 10000);
 }());
-
 
